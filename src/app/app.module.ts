1a59d8195ff8c15ce985c5e5cd7939d4291f96d5
Tracy Miller <tlmiller36@gmail.com>
2019-09-10 22:11:12 -0700

    second major commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { ShowBasicsComponent } from './show-basics/show-basics.component';
 import { ShowService } from './show/show.service';
-import {HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import { ShowSearchComponent } from './show-search/show-search.component';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShowBasicsComponent
+    ShowBasicsComponent,
+    ShowSearchComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    
+
+
   ],
   providers: [ShowService],
   bootstrap: [AppComponent]
--- src/app/show/show.service.ts
+++ src/app/show/show.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { IShowBasics } from '../ishow-basics';
-import {map} from `rxjs/operators`;
+import {map} from 'rxjs/operators';
 
 interface IShowBasicsData{
   name: string
@@ -17,9 +17,13 @@ export class ShowService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getMainShow(name: string){
+  getMainShow(search: string|number){
     return this.httpClient.get<IShowBasicsData>(`${environment.baseUrl}www.tvmaze.com/api#show-main-information/shows/:id${name}&appid=${environment.appId}`).pipe(map(data => this.transformToIShowBasics(data)))
 
   }
   private transformToIShowBasics(data: IShowBasicsData): IShowBasics { return { name: data.name, image: `http://static.tvmaze.com/uploads/images/medium_portrait/81/202627${data.image}.jpg`,summary: data.summary}}
 }
+function newFunction() {
+  return `rxjs/operators`;
+}
+
